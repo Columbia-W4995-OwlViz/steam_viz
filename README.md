63b138e51dde7c40470cc1ac4356ed34ae8d85de
Jincheng Zhang <zhjch05@live.com>
2018-11-10 18:10:42 -0500

    basic layout

--- README.md
+++ README.md
@@ -27,6 +27,8 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
+### `yarn format` to format all source code
+
 ### DON'T USE `npm run eject`
 
 **Note: this is a one-way operation. Once you `eject`, you can’t go back!**
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "antd";
+
+import { Layout, Menu, Icon } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+const { Header, Content, Footer, Sider } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Layout>
+          <Sider
+            style={{
+              overflow: "auto",
+              height: "100vh",
+              position: "fixed",
+              left: 0
+            }}
           >
-            Learn React
-          </a>
-        </header>
+            <div className="logo" />
+          </Sider>
+          <Layout style={{ marginLeft: 200 }}>
+            <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+              <div
+                style={{ padding: 24, background: "#fff", textAlign: "center" }}
+              >
+                content goes here
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }} />
+          </Layout>
+        </Layout>
       </div>
     );
   }
